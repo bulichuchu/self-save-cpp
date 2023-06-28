c355e3660ea06a86faf23d5e5ef7a445be55c0a1
bulichuchu <57799901+bulichuchu@users.noreply.github.com>
2023-06-28 23:18:31 +0800

    Delete print.cpp

--- print.cpp
+++
@@ -1,9 +0,0 @@
-#include "score statement.h"
-void print(student *head){
-    student *p =head;
-    while (p!=NULL){
-        cout<<p->num<<' '<<p->score<<endl;
-        p=p->next;
-    }
-}
-//print the list
